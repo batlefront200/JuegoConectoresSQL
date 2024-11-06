9727fc470d1298bac12893107a19d67774966348
ktr19 <159487831+ktr19@users.noreply.github.com>
2024-11-06 18:24:36 +0100

    Add files via upload

---
+++ src/main/java/daos/DAORemoto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daos;
+
+/**
+ *
+ * @author Vespertino
+ */
+public interface DAORemoto {
+
+    public void savePlayerProgress();
+
+    public void updatePlayerProgress();
+
+    public void getTopPlayers();
+
+    public void getGameStats();
+}
